0fce414e2a9fc025435263dde18f143e5ab25659
Hamed Ahmadi <hamed.raadco@gmail.com>
2023-12-03 15:50:43 +0330

    first commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function Custom404() {
+  return (
+    <h1
+      role="status"
+      className=" text-5xl flex items-center justify-center h-screen"
+    >
+      404 - Page Not Found
+    </h1>
+  );
+}
